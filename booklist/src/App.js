95e11c10263aed528c2af9fe49fcf04ad09031e9
Maksym Kaczorowski <maksym73@gmail.com>
2022-04-29 17:29:01 +0200

    Creating component BookList and BookDetails displaying information about books

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -1,11 +1,13 @@
 import BookContextProvider from "./contexts/BookContext";
 import NavBar from "./components/NavBar";
+import BookList from "./components/BookList";
 
 function App() {
   return (
     <div className="App">
       <BookContextProvider>
         <NavBar />
+        <BookList />
       </BookContextProvider>
     </div>
   );
